4911a532cbc1997fa40df341be2b0d8ed1e7c725
Manish Tewatia <ManishTewatia3020@gmail.com>
2024-11-30 18:05:27 +0530

    made logic for winning & losing but drawing is still left

--- function.js
+++ function.js
@@ -6,7 +6,13 @@ let GameBoard = [];
 
 GameLogic();
 
+let IsTrue = false;
+
+
+
 function GameLogic(){
+
+
 
 document.querySelector(`.option-selector`).addEventListener(`click`,()=>{
 
@@ -14,10 +20,21 @@ document.querySelector(`.option-display`).innerHTML = `<button class="first-one"
 
 document.querySelector(`.first-one`).addEventListener(`click`,()=>{
 
+    Accessibility();
+})
+
+})
+    
+
+}
+
+
+function Accessibility(){
 
     let CompArray = ["play-div1","play-div2","play-div3","play-div4","play-div5","play-div6","play-div7",
         "play-div8","play-div9"
     ];
+    
 
     let InputPlayer;
 
@@ -28,17 +45,17 @@ document.querySelector(`.first-one`).addEventListener(`click`,()=>{
         CompMove = `0`
     }
 
-
-
     document.querySelectorAll(`.play-div`).forEach((button)=>{
 
         button.addEventListener(`click`,()=>{
+
 
             let PlayerId = button.dataset.playerId;
 
             document.querySelector(`.${PlayerId}`).innerHTML = `X`;
 
-            reevulation();
+            
+            reevulation()
 
 
             CompArray.forEach((element,index)=>{
@@ -53,25 +70,37 @@ document.querySelector(`.first-one`).addEventListener(`click`,()=>{
 
             })
 
+            console.log(CompArray);
 
-            let CompValue = Math.floor(Math.random() * CompArray.length)
-            let CompPick = CompArray[CompValue]  
-
-            document.querySelector(`.${CompPick}`).innerHTML = `O`;
-
-            CompArray.forEach((element,index)=>{
-
-                if(CompPick === element){
-
-                    CompArray.splice(index,1)
+            if(IsTrue === false){
 
 
-                }
+                let CompValue = Math.floor(Math.random() * CompArray.length)
+                let CompPick = CompArray[CompValue]  
+    
+    
+                document.querySelector(`.${CompPick}`).innerHTML = `O`;
+    
+                CompArray.forEach((element,index)=>{
+    
+                    if(CompPick === element){
+    
+                        CompArray.splice(index,1)
+    
+    
+                    }
+    
+    
+    
+                })
+
+
+                reevulation();
 
 
 
-            })
-            
+
+            }
 
     })
 
@@ -80,69 +109,253 @@ document.querySelector(`.first-one`).addEventListener(`click`,()=>{
 })
 
 
-})
 
-})
+
+
+}
 
 
 function reevulation(){
 
-
     let TheValues = []
+       
+   document.querySelectorAll(`.play-div`).forEach((element,index)=>{
+   
+   
+   TheValues.push(element.innerHTML)
+   
+   
+   })
+   
+     ConclusionLogic(TheValues)
+   
+   }
+
+
+
+   function ConclusionLogic(TheValues){
+
+    if(TheValues[0] === TheValues[1] && TheValues[1] === TheValues[2] && TheValues[0] === `X`){
+
+        alert(`You Won`);
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+        Accessibility();
     
-document.querySelectorAll(`.play-div`).forEach((element,index)=>{
-
-
-TheValues.push(element.innerHTML)
-
-
-
-
-})
-
-console.log(TheValues)
-
-if(TheValues[0] === TheValues[1] && TheValues[1] === TheValues[2] && TheValues[0] === `X`){
-
-    alert(`You Won`) 
-    GameLogic();
-
-
-}else if (TheValues[3] === TheValues[4] && TheValues[4] === TheValues[5] && TheValues[3] === `X`){
-
-
-    alert(`You Won`)
-
-
-}else if(TheValues[6] === TheValues[7] && TheValues[7] === TheValues[8] && TheValues[6] === `X`){
-
-    alert(`You Won`)
-
-
-
-}
-
-
-
-
-
-/*
-let firstBlock = document.querySelector(`.play-div1`);
-let secondBlock = document.querySelector(`.playdiv2`);
-let thirdBlock = document.querySelector(`.playdiv3`);
-let fourthBlock = document.querySelector(`.playdiv4`);
-let fifthBlock = document.querySelector(`.playdiv5`);
-let sixthBlock = document.querySelector(`.playdiv6`);
-let seventhBlock = document.querySelector(`.playdiv7`);
-let eightBlock = document.querySelector(`.playdiv8`);
-let ninthBlock = document.querySelector(`.playdiv9`);
-*/
-
-
-
-}
-
-
+    }else if (TheValues[3] === TheValues[4] && TheValues[4] === TheValues[5] && TheValues[3] === `X`){
     
+    
+        alert(`You Won`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    }else if(TheValues[6] === TheValues[7] && TheValues[7] === TheValues[8] && TheValues[6] === `X`){
+    
+        alert(`You Won`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    }else if(TheValues[0] === TheValues[3] && TheValues[3] === TheValues[6] && TheValues[0] === `X`){
+    
+    alert(`You Won`)
+    IsTrue= true;
+    document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+        EmptyDiv.innerHTML = ``;
+
+
+
+    })
+
+    }else if(TheValues[1] === TheValues[4] && TheValues[4] === TheValues[7] && TheValues[1] === `X`){
+    
+        alert(`You Won`);
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    }else if(TheValues[2] === TheValues[5] && TheValues[5] === TheValues[8] && TheValues[2] === `X`){
+    
+        alert(`You Won`);
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    
+    }
+    
+    if(TheValues[0] === TheValues[1] && TheValues[1] === TheValues[2] && TheValues[0] === `O`){
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        }) 
+    
+    
+    }else if (TheValues[3] === TheValues[4] && TheValues[4] === TheValues[5] && TheValues[3] === `O`){
+    
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    }else if(TheValues[6] === TheValues[7] && TheValues[7] === TheValues[8] && TheValues[6] === `O`){
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    
+    }else if(TheValues[0] === TheValues[3] && TheValues[3] === TheValues[6] && TheValues[0] === `O`){
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    }else if(TheValues[1] === TheValues[4] && TheValues[4] === TheValues[7] && TheValues[1] === `O`){
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    }else if(TheValues[2] === TheValues[5] && TheValues[5] === TheValues[8] && TheValues[2] === `O`){
+    
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+    
+    
+    
+    }
+    
+    if(TheValues[0] === TheValues[4] && TheValues[4] === TheValues[8] && TheValues[0] === `X`){
+
+        alert(`You Won`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+
+
+    }
+
+    if(TheValues[2] === TheValues[4] && TheValues[4] === TheValues[6] && TheValues[2] === `X`){
+
+        alert(`You Won`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+
+
+    }
+
+    if(TheValues[0] === TheValues[4] && TheValues[4] === TheValues[8] && TheValues[0] === `O`){
+
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+
+
+    }
+
+    if(TheValues[2] === TheValues[4] && TheValues[4] === TheValues[6] && TheValues[2] === `O`){
+
+        alert(`You Lost`)
+        IsTrue= true;
+        document.querySelectorAll(`.play-div`).forEach((EmptyDiv)=>{
+
+            EmptyDiv.innerHTML = ``;
+
+
+
+        })
+
+
+    }
+
 
 }
